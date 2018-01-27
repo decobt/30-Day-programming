89297120e2f274606e936a4763fcda55883fec24
Trajche <roskoskitrajce@gmail.com>
2018-01-27 14:34:53 +0100

    EightQueens challenge

--- src/EightQueens.js
+++ src/EightQueens.js
@@ -10,13 +10,39 @@
 */
 function EightQueens(){
   //define test cases
-  this.tests = [1,2,3,4];
-  this.results = [1,2,3,4];
+  this.tests = [
+    [[2,1], [4,2], [6,3], [8,4], [3,5], [1,6], [7,7], [5,8]],
+    [[2,1], [4,3], [6,3], [8,4], [3,4], [1,6], [7,7], [5,8]],
+    [[2,1], [5,3], [6,3], [8,4], [3,4], [1,8], [7,7], [5,8]],
+    [[5,1], [3,2], [1,3], [6,4], [2,5], [8,6], [6,7], [4,8]]
+  ];
+  this.results = ["true","2,1","5,3","6,4"];
   this.getResult = function(args){
-    //add your code here
+    //define helper variable
+    var j;
+    //loop through and check queens in the input
+    for(i=0; i< args.length-1; i++){
+      //start from next element
+      j = i+1;
+      //while not end of array
+      while(j < 8){
+        //check if any any element has the same row or column
+        if(args[i][0] == args[j][0] || args[i][1] == args[j][1]){
+          //return the first queen if true
+          return args[i][0] + ',' + args[i][1];
+        }
+        //check if queens attack diagonally, the diff between coordinates is equal
+        if(Math.abs(args[i][0]-args[j][0]) == Math.abs(args[i][1]-args[j][1])){
+          //return the first queen if this is true
+          return args[i][0] + ',' + args[i][1];
+        }
+        //increment j
+        j++;
+      }
+    }
 
-    //don't forget to return your result
-    return args;
+    //don't forget to return your result if nothing else
+    return "true";
   }
 }
 
