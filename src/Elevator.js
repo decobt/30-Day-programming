ebbc4ce4137a3ff6540f3a4f8dd040dbe74721c0
Trajche <roskoskitrajce@gmail.com>
2018-02-07 21:34:08 +0100

    Elevator challenge

--- src/Elevator.js
+++ src/Elevator.js
@@ -1,6 +1,11 @@
 /*
 * Elevator Function
-* takes some input, process it, and returns some output
+* An elevator is given an input code which it then uses to go up or down floors:
+* "^" = go up one floor
+* "v" (lower-case v) = go down one floor
+* The elevator starts on floor 0.
+* Question
+* Which floor will "^v^^v^^vv^vv^vv^^vvv^^v^^^vv^^^vv^v^^vvv^^^^^v^v^v^v^vv^^vvv^^^^vvv^^vvv^^^^^vvvv^^vvv^vv^^vv^vv^v^^^" end up?
 */
 function Elevator(){
   this.tests = [1,2,3,4];
