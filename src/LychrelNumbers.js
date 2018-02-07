e23114d670b0b9264a89a6b2f41b53d5c6cfa7c0
Trajche <roskoskitrajce@gmail.com>
2018-02-07 21:34:25 +0100

    LychrelNumbers challenge

--- src/LychrelNumbers.js
+++ src/LychrelNumbers.js
@@ -1,6 +1,10 @@
 /*
 * LychrelNumbers Function
-* takes some input, process it, and returns some output
+* The Lychrel number algorithm is defined as follows: Starting with a positive integer perform the iterative process
+* of summing a number with its reversed number until you get to a palindrome number (identical to its reversed number).
+* Lychrel numbers are defined as numbers, that no matter how often you iterate this process never form a palindrome.
+* Finding a Lychrel number is an unsolved problem.
+* How many of all positive integers n < 100000 don't turn to a palindrome within the first 100 iteration steps?
 */
 function LychrelNumbers(){
   this.tests = [1,2,3,4];
