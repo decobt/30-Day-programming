02d539d0802c9840540c60681ca342b2b864409d
Trajche <roskoskitrajce@gmail.com>
2018-02-07 21:34:47 +0100

    PrimeFibonacci challenge

--- src/PrimeFibonacciSequence.js
+++ src/PrimeFibonacciSequence.js
@@ -1,6 +1,11 @@
 /*
 * PrimeFibonacciSequence Function
-* takes some input, process it, and returns some output
+* n the fibonacci sequence every number is the sum of the two preceding ones:
+* 1,1,2,3,5,8,13,21,34...
+* The sequence Fn of fibonacci numbers is defined as: Fn=Fn−1+Fn−2
+* Question
+* Find the sum of all the prime values in the fibonacci sequence, where
+* Fn < 1,000,000
 */
 function PrimeFibonacciSequence(){
   this.tests = [1,2,3,4];
