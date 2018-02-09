e4ac6d8a0dbb9e49494f110bf9086ff5dced6533
Trajche <roskoskitrajce@gmail.com>
2018-02-09 22:12:53 +0100

    solved Elevator challenge

--- src/Elevator.js
+++ src/Elevator.js
@@ -8,13 +8,29 @@
 * Which floor will "^v^^v^^vv^vv^vv^^vvv^^v^^^vv^^^vv^v^^vvv^^^^^v^v^v^v^vv^^vvv^^^^vvv^^vvv^^^^^vvvv^^vvv^vv^^vv^vv^v^^^" end up?
 */
 function Elevator(){
-  this.tests = [1,2,3,4];
-  this.results = [1,2,3,4];
+  this.tests = [
+    "^v^^v^^vv^vv^vv^^vvv^^v^^^vv^^^vv^v^^vvv^^^^^v^v^v^v^vv^^vvv^^^^vvv^^vvv^^^^^vvvv^^vvv^vv^^vv^vv^v^^^",
+    "^vv^v^^vvv^^^^^v^v^v^v^vv^^vvv^^^^vvv^^vvv^^^^^vvvv^^v",
+    "^^^^^^vvv^^vv^vvv^vvv^^vvvvv^^^^^",
+    "^^^^^vv^^"
+    ];
+  this.results = [3,2,1,5];
+  
   this.getResult = function(args){
-    //add your code here
+    //define floor variable, set to 0 floor
+    var floor = 0;
+    for(let i=0; i<args.length; i++){
+      if(args[i]==='^'){
+        //increase floor
+        floor= floor+1;
+      }else{
+        //decrease floor
+        floor= floor-1;
+      }
+    }
 
     //don't forget to return your result
-    return args;
+    return floor;
   }
 }
 
