4aecc26f4752d4fe4f6b1480f20760b276e1d595
Trajche <roskoskitrajce@gmail.com>
2018-02-11 15:13:26 +0100

    PrimeFibonacciSequence challenges

--- src/PrimeFibonacciSequence.js
+++ src/PrimeFibonacciSequence.js
@@ -5,16 +5,49 @@
 * The sequence Fn of fibonacci numbers is defined as: Fn=Fn−1+Fn−2
 * Question
 * Find the sum of all the prime values in the fibonacci sequence, where
-* Fn < 1,000,000
+* Fn < n
 */
 function PrimeFibonacciSequence(){
-  this.tests = [1,2,3,4];
-  this.results = [1,2,3,4];
+  //define test cases
+  this.tests = [1000,100,5000,4];
+  this.results = [345,112,1942,5];
+
+  //function used to test if number is a prime
+  this.test_prime = function (n){
+    if (n===1){
+      return false;
+    }
+    else if(n === 2)
+    {
+      return true;
+    }else{
+      for(var x = 2; x < n; x++){
+        if(n % x === 0){
+          return false;
+        }
+      }
+      return true;
+    }
+  }
   this.getResult = function(args){
-    //add your code here
+    //define some variables
+    var fib = [1], Fn = 1, sum = 0;
+
+    //add all fib numbers to the array
+    while(Fn < args){
+      fib.push(Fn);
+      Fn = fib[fib.length-1] + fib[fib.length-2];
+    }
+
+    //test which fib numbers are primes, calculate sum
+    for(let i=0; i<fib.length; i++){
+      if(this.test_prime(fib[i])){
+        sum = sum + fib[i];
+      }
+    }
 
     //don't forget to return your result
-    return args;
+    return sum;
   }
 }
 
