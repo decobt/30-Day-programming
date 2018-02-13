a498fc4c921e91948b70ed8174fb1dcf4cb0ba7c
Trajche <roskoskitrajce@gmail.com>
2018-02-13 22:34:59 +0100

    init MostRecurrent

---
+++ src/MostRecurrent.js
@@ -0,0 +1,21 @@
+/*
+* MostRecurrent Function
+* Given an array of numbers, find the most recurrent number.
+* For example, given [1, 2, 3, 4, 3], your algorithm should return 3, as it has the highest level of recurrence.
+* For ties, return the first found more recurrent number.
+*/
+function MostRecurrent(){
+  //define test cases
+  this.tests = [1,2,3,4];
+  this.results = [1,2,3,4];
+
+  this.getResult = function(args){
+    //add your code here
+
+    //don't forget to return your result
+    return args;
+  }
+}
+
+//make the function available to other units
+module.exports = MostRecurrent;
