e49ddb3e3993acc5e40c898ec18f5691d1548e1d
Trajche <roskoskitrajce@gmail.com>
2018-02-14 22:57:51 +0100

    solved MostRecurrent

--- src/MostRecurrent.js
+++ src/MostRecurrent.js
@@ -6,14 +6,43 @@
 */
 function MostRecurrent(){
   //define test cases
-  this.tests = [1,2,3,4];
-  this.results = [1,2,3,4];
+  this.tests = [
+      [1,5,2,6,5,2,2,1,6,8,3,2],
+      [8,8,9,9,8,7,8,9,5,6,8,3,5,9,8,9,8,9,9,9,8,6],
+      [1,2,3,4,5,1,2,3,4,5,1,2,3,1,2,1],
+      [2,4,6,8],
+      [1,2,3,4,3]
+    ];
+  this.results = [2,8,1,2,3];
 
   this.getResult = function(args){
-    //add your code here
+    //define some variables
+    var value = 0;
+    var recurrence = 0;
 
+    //loop through array
+    for(let i=0; i<args.length; i++){
+      //set count to zero
+      let count = 0;
+      //loop again through array
+      for(let j=0; j<args.length; j++){
+        //increase count is two values match
+        if(args[j]==args[i]){
+          count++;
+        }
+      }
+
+      //check if calculated count is larger than the max reccurence
+      if(recurrence<count){
+        //if it is, save value and reccurence count
+        value = args[i];
+        recurrence = count;
+      }
+    }
+
+    //console.log(value, recurrence);
     //don't forget to return your result
-    return args;
+    return value;
   }
 }
 
