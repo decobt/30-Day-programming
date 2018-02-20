16868557b5949f8482b972d0f2963cc42dd75cb6
Trajche <roskoskitrajce@gmail.com>
2018-02-20 21:48:36 +0100

    defined test cases

--- src/CorrectPath.js
+++ src/CorrectPath.js
@@ -6,7 +6,7 @@
 * for example: r = right, l = left, u = up, d = down. Your goal is to determine what characters the question marks
 * should be in order for a path to be created to go from the top left of the grid all the way to the bottom right
 * without touching previously travelled on cells in the grid.
-* 
+*
 * For example: if str is "r?d?drdd" then your program should output the final correct string that will allow a path
 * to be formed from the top left of a 5x5 grid to the bottom right. For this input, your program should therefore
 * return the string rrdrdrdd. There will only ever be one correct path and there will always be at least
@@ -14,8 +14,8 @@
 */
 function CorrectPath(){
   //define test cases
-  this.tests = [1,2,3,4];
-  this.results = [1,2,3,4];
+  this.tests = ["r?d?drdd", "drdr??rrddd?", "dddd?uuuurrr????", "rdrdr??rddd?dr", "rrrr????"];
+  this.results = ["rrdrdrdd", "drdruurrdddd", "ddddruuuurrrdddd", "rdrdruurdddldr", "rrrrdddd"];
 
   this.getResult = function(args){
     //add your code here
